ff36cd3158f31d0b45457f498dfdaaa7eef1ca7a
prspaulo <rodriguespaulo48@gmail.com>
2021-04-24 23:59:04 -0300

    Aula 3
    Home Controller
    Database Local
    Home Repository
    Models(question, awnser, user quiz)

--- devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ devquiz/lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,7 +5,16 @@ import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_wid
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
-  const QuizCardWidget({Key? key}) : super(key: key);
+  final String title;
+  final String completed;
+  final double percent;
+
+  const QuizCardWidget(
+      {Key? key,
+      required this.title,
+      required this.completed,
+      required this.percent})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +37,7 @@ class QuizCardWidget extends StatelessWidget {
             height: 20,
           ),
           Text(
-            "Gerenciamento de estado",
+            title,
             style: AppTextStyles.heading15,
           ),
           SizedBox(
@@ -39,14 +48,14 @@ class QuizCardWidget extends StatelessWidget {
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3/10",
+                  completed,
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
                   flex: 4,
                   child: ProgressIndicatorWidget(
-                    value: 0.3,
+                    value: percent,
                   ))
             ],
           )
