5e249342ad40d6c9122d5ac0be51ef4c68af68f6
prspaulo <rodriguespaulo48@gmail.com>
2021-04-26 00:04:09 -0300

    Aula 5
    tela de resultado
    animação score e outras coisas.

--- devquiz/lib/challenge/challenge_page.dart
+++ devquiz/lib/challenge/challenge_page.dart
@@ -1,6 +1,7 @@
 import 'package:devquiz/challenge/challenge_controller.dart';
 import 'package:devquiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:devquiz/challenge/widgets/quiz/quiz_widget.dart';
+import 'package:devquiz/result/result_page.dart';
 import 'package:devquiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,10 @@ import 'widgets/next_button/next_button_widget.dart';
 
 class ChallengePage extends StatefulWidget {
   final List<QuestionModel> questions;
+  final String title;
 
-  ChallengePage({Key? key, required this.questions}) : super(key: key);
+  ChallengePage({Key? key, required this.questions, required this.title})
+      : super(key: key);
 
   @override
   _ChallengePageState createState() => _ChallengePageState();
@@ -31,6 +34,13 @@ class _ChallengePageState extends State<ChallengePage> {
     if (controller.currentPage < widget.questions.length)
       pageController.nextPage(
           duration: Duration(milliseconds: 100), curve: Curves.linear);
+  }
+
+  void onSelected(bool value) {
+    if (value) {
+      controller.qtdAnwserRight++;
+    }
+    nextPage();
   }
 
   @override
@@ -64,9 +74,7 @@ class _ChallengePageState extends State<ChallengePage> {
         children: widget.questions
             .map((e) => QuizWidget(
                   question: e,
-                  onChange: () {
-                    nextPage();
-                  },
+                  onSelected: onSelected,
                 ))
             .toList(),
       ),
@@ -91,7 +99,14 @@ class _ChallengePageState extends State<ChallengePage> {
                               child: NextButtonWidget.green(
                             label: "Confirmar",
                             onTap: () {
-                              Navigator.pop(context);
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ResultPage(
+                                            result: controller.qtdAnwserRight,
+                                            title: widget.title,
+                                            length: widget.questions.length,
+                                          )));
                             },
                           )),
                       ],
--- devquiz/lib/challenge/widgets/next_button/next_button_widget.dart
+++ devquiz/lib/challenge/widgets/next_button/next_button_widget.dart
@@ -30,6 +30,13 @@ class NextButtonWidget extends StatelessWidget {
         this.onTap = onTap,
         this.label = label;
 
+  NextButtonWidget.purple({required String label, required VoidCallback onTap})
+      : this.backgroundColor = AppColors.purple,
+        this.fontColor = AppColors.white,
+        this.borderColor = AppColors.purple,
+        this.onTap = onTap,
+        this.label = label;
+
   @override
   Widget build(BuildContext context) {
     return Container(
